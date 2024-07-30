6798157b77d34b0fb1d52edbfff5312b35664c3e
AmmarYasserSaad <ammaryasser2413@gmail.com>
2024-07-30 14:15:24 +0300

    upload drum kit project

---
+++ index.js
@@ -0,0 +1,59 @@
+var buttons = document.getElementsByClassName("drum");
+function playAudio(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default:
+            console.log(key);
+            break;
+    }
+}
+function buttonAnimation(key) {
+    var activeButton = document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+    const myTimeout = setTimeout(function() {
+        buttonUnactive(key);
+    },100);
+}
+function buttonUnactive(key){
+    var activeButton = document.querySelector("."+key);
+    activeButton.classList.remove("pressed");
+}
+for (const element of buttons) {
+    element.addEventListener("click" , function(){
+        var buttonInnerHtml = element.innerHTML;
+        playAudio(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+    })
+    
+}
+document.addEventListener("keypress",function(event){
+    playAudio(event.key);
+    buttonAnimation(event.key);
+})
